81a16145dc9222c134757d20198f198e6edc6e61
fishphobiagg <dlauddn6211@icloud.com>
2023-11-14 00:20:19 +0900

    feat: 유저 지역 위험도 조회 기능 개발 [BACK-200] * 유저가 설정한 지역에 따른 위험도 조회 API 개발

--- src/main/java/com/GuardiansOfHoneyfruit/project/domain/user/controller/UserController.java
+++ src/main/java/com/GuardiansOfHoneyfruit/project/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.GuardiansOfHoneyfruit.project.domain.user.controller;
 
 import com.GuardiansOfHoneyfruit.project.domain.user.dao.UserFindDao;
 import com.GuardiansOfHoneyfruit.project.domain.user.dto.UserRegionUpdateRequest;
+import com.GuardiansOfHoneyfruit.project.domain.user.service.UserRegionService;
 import com.GuardiansOfHoneyfruit.project.domain.user.service.UserUpdateService;
 import com.GuardiansOfHoneyfruit.project.global.common.code.CommonCode;
 import com.GuardiansOfHoneyfruit.project.global.common.dto.Response;
@@ -17,16 +18,24 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserFindDao userFindDao;
+    private final UserRegionService userRegionService;
     private final UserUpdateService userUpdateService;
 
     @GetMapping("")
-    public ResponseEntity<Response>getUserInfo(@AuthenticationPrincipal OAuth2User oAuth2User){
+    public ResponseEntity<Response> getUserInfo(@AuthenticationPrincipal OAuth2User oAuth2User){
         Response response = Response.of(CommonCode.GOOD_REQUEST, userFindDao.findByUserUuId(oAuth2User.getAttributes().get("userUuid").toString()));
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/regions")
+    public ResponseEntity<Response> getUserRegionDangerLevel(@AuthenticationPrincipal OAuth2User oAuth2User){
+        Response response = userRegionService.getUserRegionDangerLevel(oAuth2User);
+        return ResponseEntity.status(response.getStatus().getHttpStatus()).body(response);
+    }
+
     @PostMapping("/regions")
     public ResponseEntity<Response> createUserRegion(@RequestBody UserRegionUpdateRequest request, @AuthenticationPrincipal OAuth2User user){
+        user.getAttributes().get("userUuid");
         Response response = userUpdateService.createUserRegion(request, user.getName());
         return ResponseEntity.status(response.getStatus().getHttpStatus()).body(response);
     }
---
+++ src/main/java/com/GuardiansOfHoneyfruit/project/domain/user/service/UserRegionService.java
@@ -0,0 +1,26 @@
+package com.GuardiansOfHoneyfruit.project.domain.user.service;
+
+import com.GuardiansOfHoneyfruit.project.domain.region.dao.RegionFindDao;
+import com.GuardiansOfHoneyfruit.project.domain.region.service.RegionDangerService;
+import com.GuardiansOfHoneyfruit.project.domain.user.code.UserCode;
+import com.GuardiansOfHoneyfruit.project.domain.user.dao.UserFindDao;
+import com.GuardiansOfHoneyfruit.project.global.common.dto.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserRegionService {
+
+    private final RegionDangerService regionDangerService;
+    private final UserFindDao userFindDao;
+    public Response getUserRegionDangerLevel(final OAuth2User oAuth2User){
+        try {
+            return regionDangerService.getDangerLevelAtSingleRegion(oAuth2User.getAttributes().get("region").toString());
+        } catch (NullPointerException e){
+            return Response.of(UserCode.USER_REGION_IS_NULL);
+        }
+    }
+
+}
